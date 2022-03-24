2c4b3e0a202ca2f62b4b167ec70d9a802123c886
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-24 13:42:37 +0300

    Create Book.js

---
+++ src/components/Book.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const Book = (props) => {
+  const { title, author } = props;
+  return (
+    <li>
+      <section>
+        <hgroup>
+          <h1>{title}</h1>
+          <h2>{author}</h2>
+        </hgroup>
+        <button type="button">Remove</button>
+      </section>
+    </li>
+  );
+};
+
+export default Book;
