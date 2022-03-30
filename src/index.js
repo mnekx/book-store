cddfb2f1cd3b354dfc1162348e7bd803e7725684
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-30 18:06:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/configurableStore';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
