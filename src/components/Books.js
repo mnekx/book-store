948ca1e13ff8798031e7016d93efd09a2343ad99
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-30 18:48:29 +0300

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,24 +1,19 @@
-/* eslint-disable import/prefer-default-export */
 /* eslint-disable react/prop-types */
-import React from 'react';
 import { connect } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
 const mapStateToProps = (state) => ({ books: state.books });
 
-const Books = (props) => {
-  const { books } = props;
-  return (
-    <div>
-      <ul>
-        {books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
-      </ul>
-      <AddBook />
-    </div>
-  );
-};
+const Books = ({ books }) => (
+  <div>
+    <ul>
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </ul>
+    <AddBook />
+  </div>
+);
 
 export default connect(mapStateToProps, null)(Books);
