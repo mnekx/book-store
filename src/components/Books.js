39bb535532f1b44c449f369518b5c8b41dbc4e07
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-31 08:04:17 +0300

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,19 +1,20 @@
 /* eslint-disable react/prop-types */
-import { connect } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
-const mapStateToProps = (state) => ({ books: state.books });
+const Books = () => {
+  const books = useSelector((state) => state.books, shallowEqual);
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </ul>
+      <AddBook />
+    </div>
+  );
+};
 
-const Books = ({ books }) => (
-  <div>
-    <ul>
-      {books.map((book) => (
-        <Book key={book.id} book={book} />
-      ))}
-    </ul>
-    <AddBook />
-  </div>
-);
-
-export default connect(mapStateToProps, null)(Books);
+export default Books;
