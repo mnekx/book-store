5dad44a0fe1fa4b00b03ef62f03fca987825159a
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-01 08:15:16 +0300

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable react/prop-types */
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import Book from './Book';
