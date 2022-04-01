a2c047a92d9b6d896623d83cece39e71a7162ac4
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-01 07:26:17 +0300

    Update configurableStore.js

--- src/redux/configurableStore.js
+++ src/redux/configurableStore.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/prefer-default-export */
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import categories from './categories/categories';
 import books from './books/books';
 
 const rootReducer = combineReducers({ categories, books });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
