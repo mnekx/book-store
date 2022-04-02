a9c3e3cadbd3fee6122312071833c9e4a95b2442
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-02 12:26:09 +0300

    style check status button

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,7 +5,11 @@ import { statusChecked } from '../redux/categories/categories';
 const Categories = () => {
   const dispatch = useDispatch();
   return (
-    <button type="button" onClick={() => dispatch(statusChecked())}>
+    <button
+      type="button"
+      className="btn update-progress-btn"
+      onClick={() => dispatch(statusChecked())}
+    >
       Check Status
     </button>
   );
